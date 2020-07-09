c8ce39eee583f26338068a84941a013638cb2cf6
Mateusz Armatys <mateusz@makenika.pl>
2020-07-09 16:40:13 +0200

    Cleanup dependencies

--- src/androidAndroidTest/kotlin/pl/makenika/graphlite/GraphLiteDatabaseTest.kt
+++ src/androidAndroidTest/kotlin/pl/makenika/graphlite/GraphLiteDatabaseTest.kt
@@ -5,10 +5,8 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import pl.makenika.graphlite.sql.AndroidSqliteDriver
 import pl.makenika.graphlite.sql.SqliteDriver
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.runner.RunWith
 
-@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 internal class GraphLiteDatabaseTest : BaseGraphLiteDatabaseTest() {
     override fun makeDriver(): SqliteDriver {
